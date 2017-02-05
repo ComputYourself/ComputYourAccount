a24433d74abdf2e9e4dcfacd73ed6ce8a5f4223d
BuildTools <unconfigured@null.spigotmc.org>
2017-02-05 18:53:10 +0100

    infos perso 2

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "infosPerso.h"
+#include <QSqlError>
+#include <QSqlDriver>
+#include <QtCore/QtCore>
 #include <QLayout>
+#include <QStackedLayout>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow) , currentWidget(new QWidget(this)){
+    ui(new Ui::MainWindow){
     ui->setupUi(this);
+    ui->centralWidget->setLayout(new QStackedLayout());
+
+    this->currentWidget = new QWidget(this);
+
     ui->centralWidget->layout()->addWidget(currentWidget);
     QObject::connect(ui->actionInfos_Perso, SIGNAL(triggered(bool)), this, SLOT(on_infosPerso_triggered()));
 }
